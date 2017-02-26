4e79c63f4f243bebe167734b9cd4166d22b57a68
unknown <daumisgod@gmail.com>
2017-02-26 17:22:09 -0500

    added in example ignore file...have successfully wrapped JPLAG...works as Runnable Jar as well (so long as you have jplag jar and config.properties) in your same directory

--- src/All/Runner.java
+++ src/All/Runner.java
@@ -5,6 +5,8 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Properties;
 
 public class Runner {
@@ -12,10 +14,18 @@ public class Runner {
 	static InputStream in = null;
 	public static void main(String[] args) throws Exception {
 		System.out.println("Welcome to Plaigarism Coupler.");
+		SanitizeSpace();
 		ReadProperties();
-		System.out.println(RunJplag(true));
+		RunJplag(true);
+		System.out.println("Finished");
+		System.exit(0);
 	}
 	
+	private static void SanitizeSpace() throws Exception {
+		System.out.println("Sanitizing Space...");
+		new ProcessBuilder("rm","-r","JplagResults").start();
+	}
+
 	private static void ReadProperties() {
 		System.out.println("Reading config.properties...");
 		try{
@@ -30,23 +40,30 @@ public class Runner {
 		
 	}
 
-	public static String RunJplag(boolean verbose) throws Exception{
-		ProcessBuilder builder = new ProcessBuilder("java","-jar","jplag-2.11.8-SNAPSHOT-jar-with-dependencies.jar","-l",
-				"java17");
+	public static void RunJplag(boolean verbose) throws Exception{
+		System.out.println("RUNNING JPLAG ON "+prop.getProperty("inputFileFolderName")+"...\n--------------------------------");
+		ArrayList<String> args = new ArrayList<String>();
+		args.add("java");args.add("-jar");args.add("jplag-2.11.8-SNAPSHOT-jar-with-dependencies.jar");
+		args.add("-l");args.add("java17");args.add("-s");args.add("-r");args.add("JPlagResults");
+		args.add("-m");args.add((int)(Float.parseFloat(prop.getProperty("plaggie.minimumFileSimilarityValueToReport"))*100)+"%");
+		//args.add("-m");args.add(prop.getProperty("plaggie.maximumDetectionResultsToReport")); //looks like this does not work with JPlag
+		if(Boolean.parseBoolean(prop.getProperty("excludeFiles"))){ //file exclusion
+			args.add("-x");args.add(prop.getProperty("excludeName"));
+		}
+		args.add(prop.getProperty("inputFileFolderName"));
+		String[] toPass = new String[args.size()];
+		ProcessBuilder builder = new ProcessBuilder(args.toArray(toPass));
 		builder.redirectErrorStream(true);
 		Process p = builder.start();
 		BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
 		String line;
-		String link = "no link";
 		while (true) {
 			line = r.readLine();
 			if (line == null) {
 				break;
 			}
-			if(line.contains("moss.stanford.edu"))link=line;
-			if(verbose)System.out.println(line);
+			System.out.println(line);
 		}
-		return link;
 	}
 
 }
